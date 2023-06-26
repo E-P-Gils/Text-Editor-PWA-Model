03e4d1524d486f20fedd39b4dff5de0d6bd2ad8c
unknown <sculptoid11@gmail.com>
2023-06-25 23:15:27 -0400

    Done except testing and deploy

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,62 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1);
+  const transaction = db.transaction('jate', 'readwrite');
+  const objectStore = transaction.objectStore('jate');
+
+  const newItem = { todo: content };
+  const request = objectStore.add(newItem);
+
+  request.onsuccess = () => {
+    console.log('Data added to the database successfully');
+  };
+
+  request.onerror = () => {
+    console.error('Error adding data to the database');
+  };
+
+  transaction.oncomplete = () => {
+    db.close();
+  };
+
+  form.reset();
+  fetchList();
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+  const transaction = db.transaction('jate', 'readonly');
+  const objectStore = transaction.objectStore('jate');
+  const request = objectStore.getAll();
+
+  request.onsuccess = () => {
+    const result = request.result;
+
+    let listItem = '';
+
+    for (let data of result) {
+      console.log(data);
+      listItem += `
+      <div class="flex-row align-center justify-space between" id="${data.id}">
+        <li class="mr-2" id="list-item" onclick="deleteItem(this)">${data.todo}</li>
+        <button class="btn btn-sm btn-info" onclick="editList(this)" id="edit-btn">Edit</button>
+      </div>
+      `;
+    }
+
+    document.getElementById('list-group').innerHTML = listItem;
+  };
+
+  request.onerror = () => {
+    console.error('Error retrieving data from the database');
+  };
+
+  transaction.oncomplete = () => {
+    db.close();
+  };
+};
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,35 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+//Here we're preventing the event, but storing it under deferredprompt for later use
+    event.preventDefault();
+    window.deferredPrompt = event;
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+      // Check if deferredPrompt is available
+  if (window.deferredPrompt) {
+    // Show the installation prompt
+    window.deferredPrompt.prompt();
+
+    // Wait for the user to respond to the prompt
+    const choiceResult = await window.deferredPrompt.userChoice;
+
+    // Reset the deferredPrompt variable
+    window.deferredPrompt = null;
+
+    // Handle the user's choice
+    if (choiceResult.outcome === 'accepted') {
+      console.log('PWA installation accepted');
+    } else {
+      console.log('PWA installation dismissed');
+    }
+  }
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('PWA installed successfully');
+});
